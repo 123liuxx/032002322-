f45887bb752bab716f74299573f9c288d0b73cca
123liuxx <112754906+123liuxx@users.noreply.github.com>
2022-10-14 16:12:08 +0800

    Add files via upload

---
+++ 逍遥骰/begin/button.js
@@ -0,0 +1,15 @@
+function myFunction1(){
+    /*url="https://www.runoob.com/jsref/obj-location.html"
+    location.assign(url)*/
+    window.location.href='file:///C:/Users/86157/Desktop/zhishaizi(工作区）/逍遥骰/双人/双人2.html';
+}
+function myFunction2()
+{
+    url='file:///C:/Users/86157/Desktop/zhishaizi(工作区）/逍遥骰/人机/人机.html';
+    location.assign(url)
+}
+function myFunction3()
+{
+    url='file:///C:/Users/86157/Desktop/zhishaizi(工作区）/逍遥骰/游戏规则/规则.html';
+    location.assign(url)
+}
